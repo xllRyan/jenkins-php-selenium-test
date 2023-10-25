9033542329a6f9b0e66371ff3ca7323a71ad1b53
xllRyan <s-zhanhui@hotmail.com>
2023-10-26 01:28:00 +0800

    changes

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v c:\\...\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v /home/ubuntu/Desktop/GitHub/jenkins-php-selenium-tes/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
+
 
 echo 'Now...'
 echo 'Visit http://localhost to see your PHP application in action.'
